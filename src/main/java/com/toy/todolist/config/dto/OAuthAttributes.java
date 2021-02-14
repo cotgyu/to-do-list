2e721e4edb31b70230ce6cfcd0eda8de67a4292d
cotgyu <v123v123s@naver.com>
2021-02-14 14:49:37 +0900

    feat : 기본 회원 기능 추가

---
+++ src/main/java/com/toy/todolist/config/dto/OAuthAttributes.java
@@ -0,0 +1,73 @@
+package com.toy.todolist.config.dto;
+
+import com.toy.todolist.user.domain.Role;
+import com.toy.todolist.user.domain.User;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Map;
+
+@Getter
+public class OAuthAttributes {
+    private Map<String, Object> attributes;
+    private String nameAttributesKey;
+    private String name;
+    private String email;
+    private String picture;
+
+    @Builder
+    public OAuthAttributes(Map<String, Object> attributes, String nameAttributesKey, String name, String email, String picture) {
+        this.attributes = attributes;
+        this.nameAttributesKey = nameAttributesKey;
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+    }
+
+    public static OAuthAttributes of(String registrationId, String userNameAttributesName, Map<String, Object> attributes) {
+
+        if ("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
+
+        return ofGoogle(userNameAttributesName, attributes);
+    }
+
+
+    public static OAuthAttributes ofGoogle(String userNameAttributesName, Map<String, Object> attributes) {
+
+        return OAuthAttributes
+                .builder()
+                .name((String) attributes.get("name"))
+                .email((String) attributes.get("email"))
+                .picture((String) attributes.get("picture"))
+                .attributes(attributes)
+                .nameAttributesKey(userNameAttributesName)
+                .build();
+    }
+
+
+    public static OAuthAttributes ofNaver(String userNameAttributesName, Map<String, Object> attributes) {
+
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes
+                .builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("picture"))
+                .attributes(response)
+                .nameAttributesKey(userNameAttributesName)
+                .build();
+    }
+
+
+    public User toEntity() {
+        return User.builder()
+                .name(name)
+                .email(email)
+                .picture(picture)
+                .role(Role.USER)
+                .build();
+    }
+}
