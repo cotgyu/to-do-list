c134b0bed611720fbb6235b41f7c0eff16f356c9
cotgyu <v123v123s@naver.com>
2021-03-07 21:25:01 +0900

    feat : 템플릿 페이지에 데이터 표시 (label, checklist)

--- src/main/java/com/toy/todolist/board/controller/InitContents.java
+++ src/main/java/com/toy/todolist/board/controller/InitContents.java
@@ -1,8 +1,6 @@
 package com.toy.todolist.board.controller;
 
-import com.toy.todolist.board.domain.Board;
-import com.toy.todolist.board.domain.Card;
-import com.toy.todolist.board.domain.Topic;
+import com.toy.todolist.board.domain.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -47,14 +45,54 @@ public class InitContents {
             Card card4 = new Card("card4", "des4", topic2);
             Card card5 = new Card("card5", "des5", topic2);
             Card card6 = new Card("card6", "des6", topic2);
-
-
             em.persist(card1);
             em.persist(card2);
             em.persist(card3);
             em.persist(card4);
             em.persist(card5);
             em.persist(card6);
+
+            CheckList checkList1 = new CheckList("checkList1", card1);
+            CheckList checkList2 = new CheckList("checkList2", card3);
+            em.persist(checkList1);
+            em.persist(checkList2);
+
+            CheckItem checkItem1 = new CheckItem("checkItem1",checkList1);
+            CheckItem checkItem2 = new CheckItem("checkItem2",checkList1);
+            CheckItem checkItem3 = new CheckItem("checkItem3",checkList1);
+            CheckItem checkItem4 = new CheckItem("checkItem4",checkList2);
+            CheckItem checkItem5 = new CheckItem("checkItem5",checkList2);
+
+            em.persist(checkItem1);
+            em.persist(checkItem2);
+            em.persist(checkItem3);
+            em.persist(checkItem4);
+            em.persist(checkItem5);
+
+            checkList1.addCheckItem(checkItem1);
+            checkList1.addCheckItem(checkItem2);
+            checkList1.addCheckItem(checkItem3);
+            checkList2.addCheckItem(checkItem4);
+            checkList2.addCheckItem(checkItem5);
+
+            Label label1 = new Label("label1", "black");
+            Label label2 = new Label("label2", "red");
+            Label label3 = new Label("label3", "blue");
+
+            em.persist(label1);
+            em.persist(label2);
+            em.persist(label3);
+
+            CardLabel cardLabel1 = new CardLabel(card1, label1);
+            CardLabel cardLabel2 = new CardLabel(card2, label2);
+            CardLabel cardLabel3 = new CardLabel(card1, label3);
+            CardLabel cardLabel4 = new CardLabel(card4, label3);
+
+            card1.addCardLabel(cardLabel1);
+            card2.addCardLabel(cardLabel2);
+            card1.addCardLabel(cardLabel3);
+            card4.addCardLabel(cardLabel4);
+
 
 
         }
