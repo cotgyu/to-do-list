303264d12ab2298d76529a8f1d0dc044b068521b
cotgyu <v123v123s@naver.com>
2021-05-19 15:18:56 +0900

    test : aws 배포테스트 - deploy.sh 수정

--- deploy.sh
+++ deploy.sh
@@ -39,5 +39,5 @@ nohup java -jar \
         -Dspring.config.location=classpath:/application.yml,/home/ec2-user/project/properties/application-oauth.properties \
         -Dspring.profiles.active=real \
         $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
-~
-~
+
+
