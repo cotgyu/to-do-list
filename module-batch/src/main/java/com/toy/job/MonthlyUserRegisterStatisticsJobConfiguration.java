613d1af1a024b8f7f0a75dcabc2edb3f4fee8c52
cotgyu <v123v123s@naver.com>
2021-06-06 18:36:52 +0900

    feat : 월별 가입자 수 통계 Job 생성

---
+++ module-batch/src/main/java/com/toy/job/MonthlyUserRegisterStatisticsJobConfiguration.java
@@ -0,0 +1,79 @@
+package com.toy.job;
+
+
+import com.toy.user.domain.MonthlyUserRegisterStats;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.database.JpaItemWriter;
+import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManagerFactory;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class MonthlyUserRegisterStatisticsJobConfiguration {
+
+    public static final String JOB_NAME = "monthlyUserRegisterStatsBatch";
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final EntityManagerFactory entityManagerFactory;
+
+    private static final int chunkSize = 100;
+
+    @Bean
+    public Job monthlyUserRegisterStatsWriterJob() {
+        return jobBuilderFactory.get(JOB_NAME)
+                .start(monthlyUserRegisterStatsWriterStep())
+                .build();
+    }
+
+    @Bean
+    public Step monthlyUserRegisterStatsWriterStep() {
+        return stepBuilderFactory.get("monthlyUserRegisterStatsWriterStep")
+                .<Object[], MonthlyUserRegisterStats>chunk(chunkSize)
+                .reader(monthlyUserRegisterStatsWriterReader())
+                .processor(monthlyUserRegisterStatsProcessor())
+                .writer(monthlyUserRegisterStatsWriter())
+                .build();
+    }
+
+    @Bean
+    public JpaPagingItemReader<Object[]> monthlyUserRegisterStatsWriterReader() {
+        return new JpaPagingItemReaderBuilder<Object[]>()
+                .name("monthlyUserRegisterStatsWriterReader")
+                .entityManagerFactory(entityManagerFactory)
+                .pageSize(chunkSize)
+                .queryString("select month(u.createdDate) as month, " +
+                        "count(u.id) as count  " +
+                        "from User u " +
+                        "where year(u.createdDate) = 2021 " +
+                        "group by month(u.createdDate)\n" +
+                        "\n")
+                .build();
+    }
+
+    @Bean
+    public ItemProcessor<Object[], MonthlyUserRegisterStats> monthlyUserRegisterStatsProcessor() {
+
+        return object -> new MonthlyUserRegisterStats(object);
+    }
+
+    @Bean
+    public JpaItemWriter<MonthlyUserRegisterStats> monthlyUserRegisterStatsWriter() {
+        JpaItemWriter<MonthlyUserRegisterStats> jpaItemWriter = new JpaItemWriter<>();
+        jpaItemWriter.setEntityManagerFactory(entityManagerFactory);
+        return jpaItemWriter;
+    }
+
+
+}
---
+++ module-domain/src/main/java/com/toy/user/domain/MonthlyUserRegisterStats.java
@@ -0,0 +1,38 @@
+package com.toy.user.domain;
+
+import com.querydsl.core.annotations.QueryProjection;
+import com.toy.common.domain.BaseEntity;
+import lombok.Data;
+import lombok.Generated;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class MonthlyUserRegisterStats{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int month;
+    private Long count;
+
+    public MonthlyUserRegisterStats(int month, Long count){
+        this.month = month;
+        this.count = count;
+    }
+
+    public MonthlyUserRegisterStats(Object[] object){
+        this.month = (Integer) object[0];
+        this.count = (Long) object[1];
+    }
+
+
+}
